34d02b78c1d14e2701a24dc376f980e6ccfd109f
n1727338h <152832207+n1727338h@users.noreply.github.com>
2024-01-22 11:39:35 +0900

    Add files via upload

--- js/commom.js
+++ js/commom.js
@@ -1,20 +1,3 @@
-// // 対象の要素を取得
-// const CONCEPT = document.querySelector(".concept_text");
-
-// //　要素がビューポート内に入っているかどうか確認してクラスと付け外しを行う関数
-// function observation() {
-//   // 要素の位置情報を取得
-//   const CONCEPTRect = CONCEPT.getBoundingClientRect();
-
-//   // 画面内に入っているかどうかを判定
-//   if (CONCEPTRect.top < window.innerHeight && CONCEPTRect.bottom >= 0) {
-//     // 画面内に入った場合、クラスを付与
-//     CONCEPT.classList.add("active"); // yourClassNameは付与したいクラス名に置き換えてください
-//   } else {
-//     // 画面外に出た場合、クラスを除去
-//     CONCEPT.classList.remove("active");
-//   }
-// }
 
 window.onload = function() {
   const spinner = document.getElementById('loading');
@@ -66,39 +49,28 @@ window.addEventListener('load', () => {
   GlowAnimeControl();
 });
 
-// スクロールイベントを追加
 window.addEventListener("scroll", observation2);
 
 const NEWS = document.querySelector(".NEWS");
 
-//　要素がビューポート内に入っているかどうか確認してクラスと付け外しを行う関数
 function observation2() {
-  // 要素の位置情報を取得
   const NEWSRect = NEWS.getBoundingClientRect();
 
-  // 画面内に入っているかどうかを判定
   if (NEWSRect.top < window.innerHeight && NEWSRect.bottom >= 0) {
-    // 画面内に入った場合、クラスを付与
-    NEWS.classList.add("active"); // yourClassNameは付与したいクラス名に置き換えてください
+    NEWS.classList.add("active");
   } else {
-    // 画面外に出た場合、クラスを除去
     NEWS.classList.remove("active");
   }
 }
 
 const SERVICE = document.querySelector(".SERVICE");
 
-//　要素がビューポート内に入っているかどうか確認してクラスと付け外しを行う関数
 function observation4() {
-  // 要素の位置情報を取得
   const SERVICERect = SERVICE.getBoundingClientRect();
 
-  // 画面内に入っているかどうかを判定
   if (SERVICERect.top < window.innerHeight && SERVICERect.bottom >= 0) {
-    // 画面内に入った場合、クラスを付与
-    SERVICE.classList.add("active"); // yourClassNameは付与したいクラス名に置き換えてください
+    SERVICE.classList.add("active"); 
   } else {
-    // 画面外に出た場合、クラスを除去
     SERVICE.classList.remove("active");
   }
 }
@@ -106,10 +78,8 @@ function observation4() {
 window.addEventListener("scroll", observation4);
 
 window.addEventListener("DOMContentLoaded", () => {
-  // 星を表示するための親要素を取得
   const stars = document.querySelector(".stars");
 
-  // 星を生成する関数
   const createStar = () => {
     const starEl = document.createElement("span");
     starEl.className = "star";
@@ -124,36 +94,36 @@ window.addEventListener("DOMContentLoaded", () => {
     stars.appendChild(starEl);
   };
 
-  // for文で星を生成する関数を指定した回数呼び出す
   for (let i = 0; i <= 500; i++) {
     createStar();
   }
 });
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
+
   direction: "horizontal",
   loop: true,
 
-  // If we need pagination
+
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //     el: '.swiper-scrollbar',
-  // },
-  slidesPerView: 4, // 一度に表示する枚数
-  speed: 6000, // ループの時間
-  allowTouchMove: false, // スワイプ無効
+  slidesPerView: 4, 
+  breakpoints: {
+    600: {
+      slidesPerView: 3,
+    }
+  },
+  speed: 6000, 
+  allowTouchMove: false, 
   autoplay: {
-    delay: 0, // 途切れなくループ
+    delay: 0, 
   },
 });
